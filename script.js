3665364c057702794ac052ccacbbeaac531eed9c
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2026-02-28 20:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 800; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseenter", () => {
+        square.style.background = getRandomColor();
+    });
+
+    square.addEventListener("mouseleave", () => {
+        square.style.background = "#1d1d1d";
+    });
+
+    container.appendChild(square);
+}
+
+function getRandomColor(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
